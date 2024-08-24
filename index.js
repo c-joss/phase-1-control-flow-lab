6eb660b1ef0e906eb60870ef4bd243d4c77d384f
Courtney Macgregor <cjoss90@hotmail.com>
2024-08-24 16:36:25 +1000

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  let greeting = "";
+  if (someValue <= 400){
+    greeting = ("This one is on me!");
+} else if (someValue > 400 && someValue < 2000){
+    greeting = ("That will be twenty bucks.");
+} else if (someValue > 2000 && someValue < 2500){
+    greeting = ("I will gladly take your thirty bucks.");
+} else if (someValue > 2500){
+    greeting = ("No can do.");
+}
+return greeting;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let service = (city === "NYC") ? "Ok, sounds good." : "No go.";
+  return service;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(charm){
+  let greeting = "";
+  switch (charm) {
+    case "generous":
+      greeting = "Thank you so much.";
+      break;
+    case "not as generous":
+      greeting = "Thank you.";
+      break;
+    case "thanks for everything":
+      greeting = "Bye.";
+      break; 
+  }
+  return greeting;
 }
